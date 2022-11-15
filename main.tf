131c87f177d27e7f03edd05adacbe0e0f2c12b06
root <root@ip-172-31-9-184.us-west-2.compute.internal>
2022-11-15 08:00:50 +0000

    terraform ecr

---
+++ main.tf
@@ -0,0 +1,171 @@
+provider "aws" {
+  access_key = var.aws-access-key
+  secret_key = var.aws-secret-key
+  region     = "eu-central-1"
+  version    = "~> 2.0"
+}
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  count             = length(var.private_subnets)
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  count                   = length(var.public_subnets)
+  map_public_ip_on_launch = true
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_nat_gateway" "main" {
+  count         = length(var.private_subnets)
+  allocation_id = element(aws_eip.nat.*.id, count.index)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  depends_on    = [aws_internet_gateway.main]
+}
+
+resource "aws_eip" "nat" {
+  count = length(var.private_subnets)
+  vpc = true
+}
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets)
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "private" {
+  count                  = length(compact(var.private_subnets))
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.main.*.id, count.index)
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+resource "aws_security_group" "alb" {
+  name   = "${var.name}-sg-alb-${var.environment}"
+  vpc_id = var.vpc_id
+
+  ingress {
+   protocol         = "tcp"
+   from_port        = 80
+   to_port          = 80
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+   protocol         = "tcp"
+   from_port        = 443
+   to_port          = 443
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+   protocol         = "-1"
+   from_port        = 0
+   to_port          = 0
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+}
+resource "aws_security_group" "ecs_tasks" {
+  name   = "${var.name}-sg-task-${var.environment}"
+  vpc_id = var.vpc_id
+
+  ingress {
+   protocol         = "tcp"
+   from_port        = var.container_port
+   to_port          = var.container_port
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+   protocol         = "-1"
+   from_port        = 0
+   to_port          = 0
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+}
+resource "aws_ecs_cluster" "main" {
+  name = "${var.name}-cluster-${var.environment}"
+}
+resource "aws_ecs_task_definition" "main" {
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  container_definitions = jsonencode([{
+   name        = "${var.name}-container-${var.environment}"
+   image       = "${var.container_image}:latest"
+   essential   = true
+   environment = var.container_environment
+   portMappings = [{
+     protocol      = "tcp"
+     containerPort = var.container_port
+     hostPort      = var.container_port
+   }]
+}
+------------------------------------------------------------------------------
+#roles to be attached
+-------------------------------------------------------------------------------
+
+resource "aws_ecs_service" "main" {
+ name                               = "${var.name}-service-${var.environment}"
+ cluster                            = aws_ecs_cluster.main.id
+ task_definition                    = aws_ecs_task_definition.main.arn
+ desired_count                      = 2
+ deployment_minimum_healthy_percent = 50
+ deployment_maximum_percent         = 200
+ launch_type                        = "FARGATE"
+ scheduling_strategy                = "REPLICA"
+
+ network_configuration {
+   security_groups  = var.ecs_service_security_groups
+   subnets          = var.subnets.*.id
+   assign_public_ip = false
+ }
+
+ load_balancer {
+   target_group_arn = var.aws_alb_target_group_arn
+   container_name   = "${var.name}-container-${var.environment}"
+   container_port   = var.container_port
+ }
+
+ lifecycle {
+   ignore_changes = [task_definition, desired_count]
+ }
+}
+
+
